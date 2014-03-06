b9f4ebe977221ca73ac65228059436c224414e6a
Yura95st <yura95st@gmail.com>
2014-03-06 18:19:58 +0200

    adds names of all tables and columns of database

--- library_database_lib/DbValues.cs
+++ library_database_lib/DbValues.cs
@@ -8,6 +8,15 @@
         public static string TABLE_BOOK_LOCATION = "book_location";
         public static string TABLE_BOOK_AUTHORS = "book_authors";
 
+        public static string TABLE_AUTHORS = "authors";
+        
+        public static string TABLE_PUBLISHERS = "publishers";
+        
+        public static string TABLE_READERS = "readers";
+        public static string TABLE_READER_BOOKS = "reader_books";
+        
+        public static string TABLE_PENALTIES = "penalties";
+
         // BOOKS table - column names
         public static string BOOKS_COLUMN_ID = "book_id";
         public static string BOOKS_COLUMN_TITLE = "title";
@@ -30,5 +39,37 @@
         // BOOK_AUTHORS table - column names
         public static string BOOK_AUTHORS_COLUMN_BOOK_ID = "book_id";
         public static string BOOK_AUTHORS_COLUMN_AUTHOR_ID = "author_id";
+
+        // AUTHORS table - column names
+        public static string AUTHORS_COLUMN_AUTHOR_ID = "author_id";
+        public static string AUTHORS_COLUMN_FIRST_NAME = "first_name";
+        public static string AUTHORS_COLUMN_LAST_NAME = "last_name";
+
+        // PUBLISHERS table - column names
+        public static string PUBLISHERS_COLUMN_PUBLISHER_ID = "pub_id";
+        public static string PUBLISHERS_COLUMN_NAME = "name";
+        public static string PUBLISHERS_COLUMN_PHONE = "phone";
+        public static string PUBLISHERS_COLUMN_ADDRESS = "address";
+        public static string PUBLISHERS_COLUMN_CITY = "city";
+
+        // READERS table - column names
+        public static string READERS_COLUMN_READER_ID = "reader_id";
+        public static string READERS_COLUMN_FIRST_NAME = "first_name";
+        public static string READERS_COLUMN_LAST_NAME = "last_name";
+        public static string READERS_COLUMN_PHONE = "phone";
+        public static string READERS_COLUMN_ADDRESS = "address";
+        public static string READERS_COLUMN_CITY = "city";
+
+        // READER_BOOKS table - column names
+        public static string READER_BOOKS_COLUMN_READER_ID = "reader_id";
+        public static string READER_BOOKS_COLUMN_BOOK_ID = "book_id";
+        public static string READER_BOOKS_COLUMN_EXP_DATE = "exp_date";
+        public static string READER_BOOKS_COLUMN_STATE = "state";
+
+        // PENALTIES table - column names
+        public static string PENALTIES_COLUMN_PENALTY_ID = "penalty_id";
+        public static string PENALTIES_COLUMN_READER_ID = "reader_id";
+        public static string PENALTIES_COLUMN_TYPE = "type";
+        public static string PENALTIES_COLUMN_EXP_DATE = "exp_date";
     }
 }
